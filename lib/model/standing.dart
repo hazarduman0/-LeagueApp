da093bfdbadddb104c886ad479d5582edea7ac99
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-07-26 23:45:42 +0300

    Update standing.dart

--- lib/model/standing.dart
+++ lib/model/standing.dart
@@ -1,57 +1,173 @@
-// // ignore_for_file: public_member_api_docs, sort_constructors_first
-// import 'dart:convert';
+class Standing {
+  Team? team;
+  List<Stats>? stats;
 
-// import 'package:lig_aplication/model/stats.dart';
-// import 'package:lig_aplication/model/team.dart';
+  Standing({this.team, this.stats});
 
-// class Standing {
-//   Team team;
-//   Stat stats;
-//   Standing({
-//     required this.team,
-//     required this.stats,
-//   });
+  Standing.fromJson(Map<String, dynamic> json) {
+    team = json['team'] != null ? Team.fromJson(json['team']) : null;
+    if (json['stats'] != null) {
+      stats = <Stats>[];
+      json['stats'].forEach((v) {
+        stats!.add(Stats.fromJson(v));
+      });
+    }
+  }
 
-//   Standing copyWith({
-//     Team? team,
-//     Stat? stats,
-//   }) {
-//     return Standing(
-//       team: team ?? this.team,
-//       stats: stats ?? this.stats,
-//     );
-//   }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (team != null) {
+      data['team'] = team!.toJson();
+    }
+    if (stats != null) {
+      data['stats'] = stats!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
 
-//   Map<String, dynamic> toMap() {
-//     return <String, dynamic>{
-//       'team': team.toJson(),
-//       'stats': stats.toJson(),
-//     };
-//   }
+class Team {
+  String? id;
+  String? uid;
+  String? location;
+  String? name;
+  String? abbreviation;
+  String? displayName;
+  String? shortDisplayName;
+  bool? isActive;
+  List<Logos>? logos;
 
-//   factory Standing.fromMap(Map<String, dynamic> map) {
-//     return Standing(
-//       team: Team.fromJson(map['team'] as Map<String,dynamic>),
-//       stats: Stat.fromJson(map['stats'] as Map<String,dynamic>),
-//     );
-//   }
+  Team(
+      {this.id,
+      this.uid,
+      this.location,
+      this.name,
+      this.abbreviation,
+      this.displayName,
+      this.shortDisplayName,
+      this.isActive,
+      this.logos});
 
-//   String toJson() => json.encode(toMap());
+  Team.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    uid = json['uid'];
+    location = json['location'];
+    name = json['name'];
+    abbreviation = json['abbreviation'];
+    displayName = json['displayName'];
+    shortDisplayName = json['shortDisplayName'];
+    isActive = json['isActive'];
+    if (json['logos'] != null) {
+      logos = <Logos>[];
+      json['logos'].forEach((v) {
+        logos!.add(Logos.fromJson(v));
+      });
+    }
+  }
 
-//   factory Standing.fromJson(String source) => Standing.fromMap(json.decode(source) as Map<String, dynamic>);
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = id;
+    data['uid'] = uid;
+    data['location'] = location;
+    data['name'] = name;
+    data['abbreviation'] = abbreviation;
+    data['displayName'] = displayName;
+    data['shortDisplayName'] = shortDisplayName;
+    data['isActive'] = isActive;
+    if (logos != null) {
+      data['logos'] = logos!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
 
-//   @override
-//   String toString() => 'Standing(team: $team, stats: $stats)';
+class Logos {
+  String? href;
+  int? width;
+  int? height;
+  String? alt;
+  List<String>? rel;
+  String? lastUpdated;
 
-//   @override
-//   bool operator ==(covariant Standing other) {
-//     if (identical(this, other)) return true;
-  
-//     return 
-//       other.team == team &&
-//       other.stats == stats;
-//   }
+  Logos(
+      {this.href,
+      this.width,
+      this.height,
+      this.alt,
+      this.rel,
+      this.lastUpdated});
 
-//   @override
-//   int get hashCode => team.hashCode ^ stats.hashCode;
-// }
+  Logos.fromJson(Map<String, dynamic> json) {
+    href = json['href'];
+    width = json['width'];
+    height = json['height'];
+    alt = json['alt'];
+    rel = json['rel'].cast<String>();
+    lastUpdated = json['lastUpdated'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['href'] = href;
+    data['width'] = width;
+    data['height'] = height;
+    data['alt'] = alt;
+    data['rel'] = rel;
+    data['lastUpdated'] = lastUpdated;
+    return data;
+  }
+}
+
+class Stats {
+  String? name;
+  String? displayName;
+  String? shortDisplayName;
+  String? description;
+  String? abbreviation;
+  String? type;
+  int? value;
+  String? displayValue;
+  String? id;
+  String? summary;
+
+  Stats(
+      {this.name,
+      this.displayName,
+      this.shortDisplayName,
+      this.description,
+      this.abbreviation,
+      this.type,
+      this.value,
+      this.displayValue,
+      this.id,
+      this.summary});
+
+  Stats.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    displayName = json['displayName'];
+    shortDisplayName = json['shortDisplayName'];
+    description = json['description'];
+    abbreviation = json['abbreviation'];
+    type = json['type'];
+    value = json['value'];
+    displayValue = json['displayValue'];
+    id = json['id'];
+    summary = json['summary'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['name'] = name;
+    data['displayName'] = displayName;
+    data['shortDisplayName'] = shortDisplayName;
+    data['description'] = description;
+    data['abbreviation'] = abbreviation;
+    data['type'] = type;
+    data['value'] = value;
+    data['displayValue'] = displayValue;
+    data['id'] = id;
+    data['summary'] = summary;
+    return data;
+  }
+}
