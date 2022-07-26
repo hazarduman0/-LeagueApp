6175c4d170a3b2e5bafcab296fa9e25237e709b9
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-07-26 23:45:50 +0300

    Update seasonCard.dart

--- lib/widget/seasonCard.dart
+++ lib/widget/seasonCard.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class SeasonCard extends StatelessWidget {
    SeasonCard({Key? key, required this.leagueName}) : super(key: key);
